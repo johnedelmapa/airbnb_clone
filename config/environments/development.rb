4bceeb3b2d678acddf3b32b1cd3e077a8ea8df34
Shiro <61719297+Kenshiro517@users.noreply.github.com>
2020-04-02 15:41:18 +0900

    finish create_basic_authentication (#1)
    Co-authored-by: 大久保健志郎 <okubokenshirou@ookubotakeshirou-no-MacBook-Air.local>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.web_console.whitelisted_ips = ["192.0.0.1/2","172.25.0.1/16", "172.0.0.1/2"]
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3003 }
 end

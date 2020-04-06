950a185a50463145ed5f72327c4fa3eaebb40cbc
Shiro <61719297+Kenshiro517@users.noreply.github.com>
2020-04-06 15:04:40 +0900

    finish facebook-authentication (#5)
    Co-authored-by: 大久保健志郎 <okubokenshirou@ookubotakeshirou-no-MacBook-Air.local>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   root 'static_page#home'
+
+  devise_for :users,
+  path: '',
+  path_names: {sign_in: 'login', sign_up: 'registration', sign_out: 'logout'},
+  controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

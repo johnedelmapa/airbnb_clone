28b2e6d2b7f65784ae9f6c70a6546fc027c0427b
JOHNEDEL MAPA <mapajohnedel@gmail.com>
2023-03-06 06:31:52 +0800

    Update codes

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -5,24 +5,17 @@ class StaticPageController < ApplicationController
   end
 
   def search
-    #step-1-assign session
     if params[:search].present?
-      #create session
       session[:loc_search] = params[:search]
     end
-    #step-2-check the session if empty or not
     if session[:loc_search] && session[:loc_search] != ""
-      #Grab al places and nearby based in the session[:loc_search]
       @room_address = Room.where(is_active: true).near(session[:loc_search],5,order: 'distance')
     else
-      #grab all places
       @room_address = Room.where(is_active: true).all
     end
-    #step-3-use ransack gem
     @search = @room_address.ransack(params[:q])
     @rooms = @search.result
     @arrRooms = @rooms.to_a
-    #step-4-check if start date and date is empty or taken
     if(params[:start_date] && params[:end_date] && !params[:start_date].empty? && !params[:end_date].empty?)
       start_date = Date.parse(params[:start_date])
       end_date = Date.parse(params[:end_date])
